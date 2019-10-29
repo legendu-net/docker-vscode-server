e8aed4d4b9bfc9ed9324ca9fe76785c0b8e1e50c
Ben Chuanlong Du <legendu.net@outlook.com>
2019-10-29 16:41:14 +0000

    update launch script

--- scripts/sys/launch.sh
+++ scripts/sys/launch.sh
@@ -4,6 +4,7 @@ export PASSWORD="$DOCKER_PASSWORD" && /usr/bin/code-server \
     --port 8080 \
     --user-data-dir /config/data \
     --extensions-dir /config/extensions \
+    --allow-https \
     --disable-telemetry \
     --disable-updates \
     --auth password \
