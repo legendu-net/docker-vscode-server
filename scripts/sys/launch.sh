6aebcb566eac62fb57d88317f752dc651a176254
Ben Chuanlong Du <dclong@users.noreply.github.com>
2020-04-13 21:45:46 -0700

    Update launch.sh

--- scripts/sys/launch.sh
+++ scripts/sys/launch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export PASSWORD="$DOCKER_PASSWORD" && /usr/bin/code-server \
+    --host 0.0.0.0 \
     --port 8080 \
     --user-data-dir /config/data \
     --extensions-dir /root/.local/share/code-server/extensions \
