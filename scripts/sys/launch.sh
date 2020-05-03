4d2c9f7c2d4221608558e81f3477d05f19ee6b8b
Ben Chuanlong Du <dclong@users.noreply.github.com>
2020-05-03 10:17:13 -0700

    Update launch.sh

--- scripts/sys/launch.sh
+++ scripts/sys/launch.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 export PASSWORD="$DOCKER_PASSWORD" && /usr/bin/code-server \
-    --host 0.0.0.0 \
-    --port 8080 \
+    --bind-addr 0.0.0.0:8080 \
     --user-data-dir /config/data \
     --extensions-dir /root/.local/share/code-server/extensions \
-    --disable-ssh \
     --disable-telemetry \
     --disable-updates \
     --auth password \
